0f2b5a7d131111f17f29403266a9d9104ea2cf41
Michael Forrester <itamazonian@gmail.com>
2021-05-18 01:15:50 -0400

    added in new lab04 for testing

--- Lab04/provider.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
- 
- region = "us-east-2"
-}
-
-
-provider "kubernetes" {
-  load_config_file = "true"
-}
--- Lab04/wp_outputs.tf
+++
@@ -1,3 +0,0 @@
-output "lb_ip" {
-  value = kubernetes_service.mysvc.status.0.load_balancer.0.ingress.0.hostname
-}
